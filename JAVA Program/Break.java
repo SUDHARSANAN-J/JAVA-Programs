2ff03ae608a8e3ef3e51ff3bbd9e9974e0405e63
Sudharsanan J <142624997+SUDHARSANAN-J@users.noreply.github.com>
2024-03-01 11:57:47 +0530

    Update Break.java

--- JAVA Program/Break.java
+++ JAVA Program/Break.java
@@ -1,8 +1,7 @@
 public class Break {
     public static void main(String [] args){
 
-        int n = 8, i;
-         for (i = 1; i <= n; i++)
+
          {
             System.out.println(i);
             if (i == 4)
